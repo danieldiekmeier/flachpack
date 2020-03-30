b447e217d278c37fb5c23c3655391da081383943
Daniel Diekmeier <fax@danieldiekmeier.de>
2020-03-30 17:24:41 +0200

    Great big overhaul

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Flachpack
 
-Flachpack pipes a directory of HTML files through [EJS](https://github.com/mde/ejs) and [HTML Minifier](https://github.com/kangax/html-minifier), resulting in a flat directory of _only_ HTML files. Use it to build the smallest possible websites.
+Flachpack pipes a directory of HTML files through [EJS](https://github.com/mde/ejs), [Minifier](https://github.com/kangax/html-minifier) and [Terser](https://github.com/terser/terser), resulting in a flat directory of HTML files. Use it to build tiny websites!
 
 I built it for my tiny [personal website](https://danieldiekmeier.de).
 
@@ -9,11 +9,33 @@ npm i flachpack -g
 
 flachpack -o dist src
 # -o / --out:    The output directory
-# last argument: The folder containing some HTML files.
+# -w / --watch:  Enable watch mode and serve over BrowserSync
+# last argument: The folder containing the HTML files
 ```
 
-Look at [the repo for danieldiekmeier.de](https://github.com/danieldiekmeier/danieldiekmeier.de) for an example.
+An example `src/index.html`:
 
-## Plans for the future:
+```html
+<html>
+  <head>
+    <style type="text/css">
+      /* inline Sass, SCSS or plain SCSS */
+      <%- includeSass('main.scss') %>
+    </style>
 
-It would be nice to support inlining images as base64. Currently, I don't use an image on [danieldiekmeier.de](https://danieldiekmeier.de), so it's not my priority.
+    <script>
+      // inline JavaScript
+      <%- include('./script.js') %>
+    </script>
+  </head>
+
+  <body>
+    <!-- reuse some parts! -->
+    <%- include('./parts/header.html') %>
+
+    <h1>Hello World!</h1>
+  </body>
+</html>
+```
+
+Look at [the repo for danieldiekmeier.de](https://github.com/danieldiekmeier/danieldiekmeier.de) for a bigger example.
--- index.js
+++ index.js
@@ -1,44 +1,114 @@
 #!/usr/bin/env node
 
 const jetpack = require('fs-jetpack')
+const Terser = require('terser')
 const ejs = require('ejs')
 const path = require('path')
 const minimist = require('minimist')
 const { minify } = require('html-minifier')
 
 const argv = require('minimist')(process.argv.slice(2), {
+  boolean: ['watch'],
   alias: {
+    watch: 'w',
     out: 'o'
   }
 })
 
+const DONE_MESSAGE = 'Done! 🎉 Compiled in'
 const input = argv._[0]
 const src = path.resolve(process.cwd(), input)
 const dist = path.resolve(process.cwd(), argv.out)
 
-const files = jetpack.find(src, { matching: '*.html', recursive: false })
-  .map(fileName => path.resolve(process.cwd(), fileName))
-  .map(fileName => ({
-    fileName: fileName,
-    distPath: path.resolve(dist, path.basename(fileName)),
-    content: minify(ejs.render(jetpack.read(fileName), {
-      filename: fileName
-    }), {
-      minifyCSS: true,
-      minifyJS: true,
-      removeAttributeQuotes: true,
-      removeComments: true,
-      removeScriptTypeAttributes: true,
-      collapseWhitespace: true,
-      conservativeCollapse: false
-    })
-  }))
+if (argv.watch) {
+  const chokidar = require('chokidar')
+  const BrowserSync = require('browser-sync')
+  const browserSync = BrowserSync.create()
+  browserSync.init({
+    server: dist,
+    ui: false,
+    open: false,
+    notify: false
+  })
 
-// Remove and add Output Directory
-jetpack.dir(dist)
+  const watcher = chokidar.watch(src, { ignoreInitial: true })
 
-files.forEach(file => {
-  jetpack.write(file.distPath, file.content)
-})
+  watcher.on('ready', () => {
+    compile(browserSync)
+  })
 
-console.log('Done! 🎉')
+  watcher.on('all', (event, path) => {
+    console.log(event, path)
+    compile(browserSync)
+  })
+} else {
+  compile()
+}
+
+function includeSass(fileName) {
+  const file = path.resolve(src, fileName)
+  const Sass = require('sass')
+  return Sass.renderSync({ file }).css.toString()
+}
+
+function compile(browserSync) {
+  console.time(DONE_MESSAGE)
+
+  const files = jetpack
+    .find(src, { matching: '*.html', recursive: false })
+    .map(fileName => path.resolve(process.cwd(), fileName))
+    .map(fileName => ({
+      fileName: fileName,
+      distPath: path.resolve(dist, path.basename(fileName)),
+      content: minify(
+        ejs.render(
+          jetpack.read(fileName),
+          {
+            includeSass
+          },
+          {
+            filename: fileName
+          }
+        ),
+        {
+          minifyCSS: !argv.watch,
+          minifyJS: argv.watch
+            ? false
+            : (text, inline) => {
+                const result = Terser.minify(text, { warnings: true })
+                if (result.warnings) console.log(result.warnings)
+                if (result.error) {
+                  console.log(text)
+                  throw result.error
+                }
+                return result.code
+              },
+          removeAttributeQuotes: true,
+          removeComments: true,
+          removeScriptTypeAttributes: true,
+          collapseWhitespace: true,
+          conservativeCollapse: false
+        }
+      )
+    }))
+
+  // Ensure empty output directory exists
+  jetpack.dir(dist, { empty: true })
+
+  jetpack.copy(src, dist, {
+    overwrite: true,
+    matching: ['html', 'css', 'js', 'scss'].map(
+      extension => `!**/*.${extension}`
+    )
+  })
+
+  files.forEach(file => {
+    jetpack.write(file.distPath, file.content)
+  })
+
+  if (browserSync) {
+    browserSync.reload('*.html')
+  }
+
+  console.timeEnd(DONE_MESSAGE)
+}
