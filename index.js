ec2e8b2d7714a20a3a886bac36abab2ab6ff255f
Daniel Diekmeier <fax@danieldiekmeier.de>
2023-04-06 12:48:49 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 #!/usr/bin/env node
 
-const jetpack = require('fs-jetpack')
-const Terser = require('terser')
-const ejs = require('ejs')
-const path = require('path')
-const minimist = require('minimist')
-const { minify } = require('html-minifier')
+import jetpack from 'fs-jetpack'
+import ejs from 'ejs'
+import path from 'path'
+import minimist from 'minimist'
+import { minify } from 'html-minifier'
+import makeSynchronous from 'make-synchronous'
+import chokidar from 'chokidar'
+import BrowserSync from 'browser-sync'
+import Sass from 'sass'
 
-const argv = require('minimist')(process.argv.slice(2), {
+const terserMinify = makeSynchronous(async (code) => {
+  const minifyTerserAsync = await import('terser').then((m) => m.minify)
+
+  const result = await minifyTerserAsync(code, { warnings: true })
+  if (result.warnings) console.log(result.warnings)
+  if (result.error) {
+    console.log(code)
+    throw result.error
+  }
+  return result.code
+})
+
+const argv = minimist(process.argv.slice(2), {
   boolean: ['watch'],
   alias: {
     watch: 'w',
-    out: 'o'
-  }
+    out: 'o',
+  },
 })
 
 const DONE_MESSAGE = 'Done! 🎉 Compiled in'
@@ -21,14 +36,12 @@ const src = path.resolve(process.cwd(), input)
 const dist = path.resolve(process.cwd(), argv.out)
 
 if (argv.watch) {
-  const chokidar = require('chokidar')
-  const BrowserSync = require('browser-sync')
   const browserSync = BrowserSync.create()
   browserSync.init({
     server: dist,
     ui: false,
     open: false,
-    notify: false
+    notify: false,
   })
 
   const watcher = chokidar.watch(src, { ignoreInitial: true })
@@ -47,7 +60,6 @@ if (argv.watch) {
 
 function includeSass(fileName) {
   const file = path.resolve(src, fileName)
-  const Sass = require('sass')
   return Sass.renderSync({ file }).css.toString()
 }
 
@@ -56,41 +68,32 @@ function compile(browserSync) {
 
   const files = jetpack
     .find(src, { matching: '*.html', recursive: false })
-    .map(fileName => path.resolve(process.cwd(), fileName))
-    .map(fileName => ({
-      fileName: fileName,
-      distPath: path.resolve(dist, path.basename(fileName)),
-      content: minify(
-        ejs.render(
-          jetpack.read(fileName),
-          {
-            includeSass
-          },
-          {
-            filename: fileName
-          }
-        ),
+    .map((fileName) => path.resolve(process.cwd(), fileName))
+    .map((fileName) => {
+      const content = ejs.render(
+        jetpack.read(fileName),
         {
+          includeSass,
+        },
+        {
+          filename: fileName,
+        }
+      )
+
+      return {
+        fileName: fileName,
+        distPath: path.resolve(dist, path.basename(fileName)),
+        content: minify(content, {
           minifyCSS: !argv.watch,
-          minifyJS: argv.watch
-            ? false
-            : (text, inline) => {
-                const result = Terser.minify(text, { warnings: true })
-                if (result.warnings) console.log(result.warnings)
-                if (result.error) {
-                  console.log(text)
-                  throw result.error
-                }
-                return result.code
-              },
+          minifyJS: argv.watch ? false : terserMinify,
           removeAttributeQuotes: true,
           removeComments: true,
           removeScriptTypeAttributes: true,
           collapseWhitespace: true,
-          conservativeCollapse: false
-        }
-      )
-    }))
+          conservativeCollapse: false,
+        }),
+      }
+    })
 
   // Ensure empty output directory exists
   jetpack.dir(dist, { empty: true })
@@ -98,11 +101,11 @@ function compile(browserSync) {
   jetpack.copy(src, dist, {
     overwrite: true,
     matching: ['html', 'css', 'js', 'scss'].map(
-      extension => `!**/*.${extension}`
-    )
+      (extension) => `!**/*.${extension}`
+    ),
   })
 
-  files.forEach(file => {
+  files.forEach((file) => {
     jetpack.write(file.distPath, file.content)
   })
 
